9f3ed77a83cca7c578725d22f1d70b7f2ed71556
Nasa Nguyen <nasa8x@gmail.com>
2018-04-05 15:55:24 +0700

    yescrypt

--- multihashing.cc
+++ multihashing.cc
@@ -69,7 +69,7 @@ NAN_METHOD(lyra2rev2) {
 
     uint32_t input_len = Buffer::Length(target);
 
-    lyra2rev2_hash(input, output);
+    lyra2rev2_hash(input, output, input_len);
 
     info.GetReturnValue().Set(Nan::NewBuffer(output, 32).ToLocalChecked());
 }
